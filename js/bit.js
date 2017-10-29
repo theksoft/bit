3fdc6cee5d2e77be81254179d8414bb06ed0f799
Thek <soft.thek@gmail.com>
2017-10-29 23:38:20 +0100

    : Create initial files
    Task-Url:

---
+++ js/bit.js
@@ -0,0 +1,11 @@
+/**
+ * Boardgame image tool (BIT)
+ * Copyright 2017 Herve Retaureau
+ */
+
+var bit = (function() {
+  'use strict';
+
+  function $(s) { return document.getElementById(s); }
+
+})(); /* bit */
